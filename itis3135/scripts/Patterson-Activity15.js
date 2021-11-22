66f01b5c535d0914f9855a4f878df677ec87147a
JustinPatterson319 <jpatte88@uncc.edu>
2021-11-22 14:00:43 -0500

    Update Patterson-Activity15.js

--- itis3135/scripts/Patterson-Activity15.js
+++ itis3135/scripts/Patterson-Activity15.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	$.getJSON('/json_files/facultyList.json', function(data) {
+	$.getJSON('json_files/facultyList.json', function(data) {
 		$.each(data, function() {
 			$.each(this, function(key, value) {
 				$("#faculty").append("<img src="+"'"+value.image+"'><br>"+
 				"<h2>" + value.full_name + "</h3>" +
-				"<h3>" + value.title + "</h3>" +
-				"<p>" + value.tag_line + "</p><br>");
+				"<h3>" + value.department + "</h3>" +
+				"<p>" + value.bio + "</p><br>");
 				});
 			});
 		});
