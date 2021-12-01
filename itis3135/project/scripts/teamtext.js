7a5889e4328ef3a5cc4afca2c1f8f77b0a1be79a
JustinPatterson319 <jpatte88@uncc.edu>
2021-12-01 12:59:21 -0500

    add ajax

---
+++ itis3135/project/scripts/teamtext.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $.ajax({
+        type: "get",
+        url: "components/team.json",
+        beforeSend: function() {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) {
+            $("#team").html("");
+            $.each(data, function() {
+            $.each(this, function(key, text) {
+            	$("#team").append(text.description);
+            });
+          });
+        }
+    });
+});
